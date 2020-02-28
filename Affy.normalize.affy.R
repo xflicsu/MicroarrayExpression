dc84a8a989f47313151b4a857bbfaa80637d8572
Xianfeng Li <xfliwz@gmail.com>
2020-02-28 21:33:49 +0800

    Update Affy.normalize.affy.R

--- Affy.normalize.affy.R
+++ Affy.normalize.affy.R
@@ -4,5 +4,12 @@ cmd="tar xf GSE27366/GSE27366_RAW.tar -C GSE27366"
 system(cmd, intern = T)
 library(affy)
 data <- ReadAffy(celfile.path="GSE27366") 
-eset <- rma(data)
+
+#eset <- rma(data)
+
+eset <- expresso(data,bgcorrect.method="rma",
+normalize.method="quantiles",
+pmcorrect.method="pmonly",
+summary.method="medianpolish")
+
 write.exprs(eset,file="E-GEOD-27366.txt")
